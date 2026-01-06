9c31821fad032a2890d225171176230dc4d745f4
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-05 21:42:00 -0700

    LFS Commit
    Another commit

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/INV_PlayerController.cpp
@@ -0,0 +1,91 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/INV_PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Inventory.h"
+#include "Kismet/GameplayStatics.h"
+#include "UI/HUD/INV_HUDWidget.h"
+
+AINV_PlayerController::AINV_PlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AINV_PlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	TraceForItem();
+}
+
+void AINV_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem { ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()) };
+	if (IsValid(Subsystem))
+	{
+		for (UInputMappingContext* CurrentContext : DefaultIMCs)
+		Subsystem->AddMappingContext(CurrentContext, 0);
+	}
+	
+	CreateHUDWidget();
+}
+
+void AINV_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	UEnhancedInputComponent* EnhancedInputComponent { CastChecked<UEnhancedInputComponent>(InputComponent) };
+	
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &ThisClass::PrimaryInteract);
+}
+
+void AINV_PlayerController::PrimaryInteract()
+{
+	UE_LOGFMT(LogInventory, Log, "Primary interact triggered");
+}
+
+void AINV_PlayerController::CreateHUDWidget()
+{
+	if (!IsLocalController()) return;
+	
+	if (IsValid(HUDWidgetClass))
+	{
+		HUDWidget = CreateWidget<UINV_HUDWidget>(GetWorld(), HUDWidgetClass);
+		HUDWidget->AddToViewport();
+	}
+}
+
+void AINV_PlayerController::TraceForItem()
+{
+	if (!IsValid(GEngine) || !IsValid(GEngine->GameViewport)) return;
+	
+	FVector2D ViewportSize;
+	GEngine->GameViewport->GetViewportSize(ViewportSize);
+	const FVector2D ViewportCenter = ViewportSize / 2.f;
+	FVector TraceStart;
+	FVector Forward;
+	if (!UGameplayStatics::DeprojectScreenToWorld(this, ViewportCenter, TraceStart, Forward)) return;
+	
+	const FVector TraceEnd = TraceStart + Forward * TraceLength;
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+	
+	LastActor = ThisActor;
+	ThisActor = HitResult.GetActor();
+	
+	if (ThisActor == LastActor) return;
+	
+	if (ThisActor.IsValid())
+	{
+		UE_LOG(LogInventory, Warning, TEXT("Item trace hit actor: %s"), *ThisActor->GetName());
+	}
+	
+	if (LastActor.IsValid())
+	{
+		UE_LOG(LogInventory, Warning, TEXT("Item trace left actor: %s"), *LastActor->GetName());
+	}
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/INV_PlayerController.h
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "INV_PlayerController.generated.h"
+
+class UINV_HUDWidget;
+class UInputAction;
+class UInputMappingContext;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AINV_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AINV_PlayerController();
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void PrimaryInteract();
+	void CreateHUDWidget();
+	void TraceForItem();
+	
+	UPROPERTY(EditDefaultsOnly, Category="INV|Input")
+	TArray<TObjectPtr<UInputMappingContext>> DefaultIMCs;
+	
+	UPROPERTY(EditDefaultsOnly, Category="INV|Input")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category="INV|HUD")
+	TSubclassOf<UINV_HUDWidget> HUDWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UINV_HUDWidget> HUDWidget;
+	
+	UPROPERTY(EditDefaultsOnly, Category="INV|Trace")
+	double TraceLength { 500.f };
+	
+	UPROPERTY(EditDefaultsOnly, Category="INV|Trace")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+	
+	TWeakObjectPtr<AActor> ThisActor { nullptr };
+	TWeakObjectPtr<AActor> LastActor { nullptr };
+};
