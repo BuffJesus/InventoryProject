948a70c100cbdba1cc59b7470fc0861394d4cf56
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-10 13:15:55 -0700

    Add button bindings and active grid switching logic to `UINV_SpatialInventory`

--- Plugins/Inventory/Source/Inventory/Private/UI/Inventory/Spatial/INV_SpatialInventory.cpp
+++ Plugins/Inventory/Source/Inventory/Private/UI/Inventory/Spatial/INV_SpatialInventory.cpp
@@ -2,3 +2,44 @@
 
 
 #include "UI/Inventory/Spatial/INV_SpatialInventory.h"
+#include "Components/Button.h"
+#include "Components/WidgetSwitcher.h"
+#include "UI/Inventory/Spatial/INV_InventoryGrid.h"
+
+void UINV_SpatialInventory::ShowEquippableGrid()
+{
+	SetActiveGrid(Grid_Equippable, Button_Equippable);
+}
+
+void UINV_SpatialInventory::ShowConsumableGrid()
+{
+	SetActiveGrid(Grid_Consumable, Button_Consumable);
+}
+
+void UINV_SpatialInventory::ShowCraftableGrid()
+{
+	SetActiveGrid(Grid_Craftable, Button_Craftable);
+}
+
+void UINV_SpatialInventory::DisableButton(UButton* Button)
+{
+	Button_Equippable->SetIsEnabled(true);
+	Button_Consumable->SetIsEnabled(true);
+	Button_Craftable->SetIsEnabled(true);
+	Button->SetIsEnabled(false);
+}
+
+void UINV_SpatialInventory::SetActiveGrid(UINV_InventoryGrid* Grid, UButton* Button)
+{
+	DisableButton(Button);
+	Switcher->SetActiveWidget(Grid);
+}
+
+void UINV_SpatialInventory::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+	
+	Button_Equippable->OnClicked.AddDynamic(this, &ThisClass::ShowEquippableGrid);
+	Button_Consumable->OnClicked.AddDynamic(this, &ThisClass::ShowConsumableGrid);
+	Button_Craftable->OnClicked.AddDynamic(this, &ThisClass::ShowCraftableGrid);
+}
--- Plugins/Inventory/Source/Inventory/Public/UI/Inventory/Spatial/INV_SpatialInventory.h
+++ Plugins/Inventory/Source/Inventory/Public/UI/Inventory/Spatial/INV_SpatialInventory.h
@@ -6,6 +6,7 @@
 #include "UI/Inventory/Base/INV_InventoryBase.h"
 #include "INV_SpatialInventory.generated.h"
 
+class UButton;
 class UWidgetSwitcher;
 class UINV_InventoryGrid;
 /**
@@ -16,9 +17,24 @@ class INVENTORY_API UINV_SpatialInventory : public UINV_InventoryBase
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeOnInitialized() override;
+	
 private:	
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UWidgetSwitcher> Switcher;
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UINV_InventoryGrid> Grid_Equippable;
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UINV_InventoryGrid> Grid_Consumable;
 	UPROPERTY(meta = (BindWidget)) TObjectPtr<UINV_InventoryGrid> Grid_Craftable;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Equippable;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Consumable;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Craftable;
+	
+	UFUNCTION() void ShowEquippableGrid();
+	UFUNCTION() void ShowConsumableGrid();
+	UFUNCTION() void ShowCraftableGrid();
+	
+	void DisableButton(UButton* Button);
+	void SetActiveGrid(UINV_InventoryGrid* Grid, UButton* Button);
 };
+
+
