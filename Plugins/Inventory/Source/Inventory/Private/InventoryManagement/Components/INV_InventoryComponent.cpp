d47908eef09b6f69c5f8a2d62aad627b6aa8a510
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-10 11:45:55 -0700

    Add `UINV_SpatialInventory`, `UINV_InventoryBase`, and `UINV_InventoryComponent` classes for inventory management system integration

---
+++ Plugins/Inventory/Source/Inventory/Private/InventoryManagement/Components/INV_InventoryComponent.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "InventoryManagement/Components/INV_InventoryComponent.h"
+#include "UI/Inventory/Base/INV_InventoryBase.h"
+
+UINV_InventoryComponent::UINV_InventoryComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UINV_InventoryComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	ConstructInventory();
+}
+
+void UINV_InventoryComponent::ConstructInventory()
+{
+	OwningController = Cast<APlayerController>(GetOwner());
+	checkf(OwningController.IsValid(), TEXT("OwningController cannot be null"));
+	if (!OwningController->IsLocalController()) return;
+	
+	Inventory = CreateWidget<UINV_InventoryBase>(OwningController.Get(), InventoryClass);
+	checkf(Inventory, TEXT("Inventory cannot be null"));
+	Inventory->AddToViewport();
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/InventoryManagement/Components/INV_InventoryComponent.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "INV_InventoryComponent.generated.h"
+
+
+class UINV_InventoryBase;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable)
+class INVENTORY_API UINV_InventoryComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UINV_InventoryComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	TWeakObjectPtr<APlayerController> OwningController { nullptr };
+	
+	void ConstructInventory();
+	
+	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
+	TSubclassOf<UINV_InventoryBase> InventoryClass { nullptr };
+	
+	UPROPERTY()
+	TObjectPtr<UINV_InventoryBase> Inventory { nullptr };
+};
