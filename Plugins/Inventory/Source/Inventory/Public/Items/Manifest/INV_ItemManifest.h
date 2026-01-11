0f092ad1d15825931df2a3125ca4d3f3b20da1f5
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-10 22:35:41 -0700

    Add `ItemType` property using `FGameplayTag` to `INV_ItemManifest`

--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/INV_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/INV_ItemManifest.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Types/INV_GridTypes.h"
+#include "GameplayTagContainer.h"
 #include "INV_ItemManifest.generated.h"
 
 class UINV_InventoryItem;
@@ -16,9 +17,13 @@ struct INVENTORY_API FINV_ItemManifest
 	GENERATED_BODY()
 	UINV_InventoryItem* CreateItem(UObject* NewOuter);
 	EINV_ItemCategory GetItemCategory() const { return ItemCategory; }
+	FGameplayTag GetItemType() const { return ItemType; }
 	
 private:
 	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
 	EINV_ItemCategory ItemCategory { EINV_ItemCategory::None };
+	
+	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
+	FGameplayTag ItemType;
 };
 
