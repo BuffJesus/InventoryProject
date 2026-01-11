36f5429830ff24147bf0e3386f60dffa7514a09c
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-10 22:34:05 -0700

    Add `GameplayTags` support for inventory system and define item-specific tags
    - Added `GameplayTags` module dependency in `Inventory.Build.cs`.
    - Introduced `INV_ItemTags` with declared and defined tags for weapons, cloaks, masks, potions, and craftable items.
    - Updated `BP_INV_Potion_Small_Red` asset.

--- Plugins/Inventory/Source/Inventory/Inventory.Build.cs
+++ Plugins/Inventory/Source/Inventory/Inventory.Build.cs
@@ -40,7 +40,8 @@ public class Inventory : ModuleRules
 				"SlateCore", 
 				"EnhancedInput",
 				"UMG",
-				"NetCore"
+				"NetCore",
+				"GameplayTags"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
---
+++ Plugins/Inventory/Source/Inventory/Private/Items/INV_ItemTags.cpp
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/INV_ItemTags.h"
+
+namespace GameItems
+{
+	namespace Equipment
+	{
+		namespace Weapons
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Axe, "GameItems.Equipment.Weapons.Axe", "Axe weapon type")
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(Sword, "GameItems.Equipment.Weapons.Sword", "Sword weapon type")
+		}
+		namespace Cloaks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(RedCloak, "GameItems.Equipment.Cloaks.RedCloak", "A majestic red cloak")
+		}
+		namespace Masks
+		{
+			UE_DEFINE_GAMEPLAY_TAG_COMMENT(SteelMask, "GameItems.Equipment.Masks.SteelMask", "A sturdy steel mask")
+		}
+	}
+
+	namespace Consumables
+	{
+		namespace Potions
+		{
+			namespace Red
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Consumables.Potions.Red.Small", "Small Red Potion")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Consumables.Potions.Red.Large", "Large Red Potion")
+			}
+			namespace Blue
+			{
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Small, "GameItems.Consumables.Potions.Blue.Small", "Small Blue Potion")
+				UE_DEFINE_GAMEPLAY_TAG_COMMENT(Large, "GameItems.Consumables.Potions.Blue.Large", "Large Blue Potion")
+			}
+		}
+	}
+
+	namespace Craftables
+	{
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(FireFernFruit, "GameItems.Craftables.FireFernFruit", "Crafting ingredient: Fire Fern Fruit")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(LuminDaisy, "GameItems.Craftables.LuminDaisy", "Crafting ingredient: Lumin Daisy")
+		UE_DEFINE_GAMEPLAY_TAG_COMMENT(ScorchPedalBlossom, "GameItems.Craftables.ScorchPedalBlossom", "Crafting ingredient: Scorch Pedal Blossom")
+	}
+}
