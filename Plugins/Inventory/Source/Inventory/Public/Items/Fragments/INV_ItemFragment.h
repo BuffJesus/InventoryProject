1292a05163731acd8ac83bc7e087c5b93693da15
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-11 17:11:27 -0700

    Add `INV_ItemFragment` and `FragmentTags` to support modular item design
    - Introduced `INV_ItemFragment` and `FINV_GridFragment` for fragment-based item configuration.
    - Added `FragmentTags` namespace with predefined tags, including `GridFragment`.
    - Updated `INV_ItemManifest` to include `Fragments` array for modular data.

---
+++ Plugins/Inventory/Source/Inventory/Public/Items/Fragments/INV_ItemFragment.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "INV_ItemFragment.generated.h"
+
+USTRUCT(BlueprintType)
+struct FInv_ItemFragment
+{
+	GENERATED_BODY()
+	
+	FInv_ItemFragment() {}
+	FInv_ItemFragment(const FInv_ItemFragment&) = default;
+	FInv_ItemFragment& operator=(const FInv_ItemFragment&) = default;
+	FInv_ItemFragment(FInv_ItemFragment&&) = default;
+	FInv_ItemFragment& operator=(FInv_ItemFragment&&) = default;
+	virtual ~FInv_ItemFragment() {}
+	
+	FORCEINLINE FGameplayTag GetFragmentTag() const { return FragmentTag; }
+	FORCEINLINE void SetFragmentTag(FGameplayTag Tag) { FragmentTag = Tag; }
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
+	FGameplayTag FragmentTag { FGameplayTag::EmptyTag };
+};
+
+USTRUCT(BlueprintType)
+struct FINV_GridFragment : public FInv_ItemFragment
+{
+	GENERATED_BODY()
+	FIntPoint GetGridSize() const { return GridSize; }
+	float GetGridPadding() const { return GridPadding; }
+	void SetGridSize(FIntPoint Size) { GridSize = Size; }
+	void SetGridPadding(float Padding) { GridPadding = Padding; }
+	
+private:
+	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
+	FIntPoint GridSize { 1, 1 };
+	
+	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
+	float GridPadding { 0.f };
+};
--- Plugins/Inventory/Source/Inventory/Public/Items/Manifest/INV_ItemManifest.h
+++ Plugins/Inventory/Source/Inventory/Public/Items/Manifest/INV_ItemManifest.h
@@ -5,8 +5,10 @@
 #include "CoreMinimal.h"
 #include "Types/INV_GridTypes.h"
 #include "GameplayTagContainer.h"
+#include "StructUtils/InstancedStruct.h"
 #include "INV_ItemManifest.generated.h"
 
+struct FInv_ItemFragment;
 class UINV_InventoryItem;
 
 // The item manifest contains all necessary data to create a new inventory item
@@ -20,6 +22,9 @@ struct INVENTORY_API FINV_ItemManifest
 	FGameplayTag GetItemType() const { return ItemType; }
 	
 private:
+	UPROPERTY(EditAnywhere,Category="Inventory",meta = (ExcludeBaseStruct, BaseStruct = "/Script/Inventory.Inv_ItemFragment"))
+	TArray<TInstancedStruct<FInv_ItemFragment>> Fragments;
+	
 	UPROPERTY(EditAnywhere, Category = "INV|Inventory")
 	EINV_ItemCategory ItemCategory { EINV_ItemCategory::None };
 	
