430ec1abfea6ce7dac8b36beb8e0956cec8cad87
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-01-20 18:50:32 -0700

    Add `UINV_HoverItem` widget for displaying and managing hovered inventory items
    - Introduced `UINV_HoverItem` for handling mouse-following hover visuals and stack display.
    - Implemented functions to update stack count, manage item visuals, and retrieve item-related data.

---
+++ Plugins/Inventory/Source/Inventory/Private/UI/Inventory/HoverItem/INV_HoverItem.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Inventory/HoverItem/INV_HoverItem.h"
+#include "Components/TextBlock.h"
+#include "Components/Image.h"
+#include "Items/INV_InventoryItem.h"
+
+void UINV_HoverItem::SetImageBrush(const FSlateBrush& Brush) const
+{
+	Image_Icon->SetBrush(Brush);
+}
+
+void UINV_HoverItem::UpdateStackCount(const int32 Count) const
+{
+	if (Count > 0)
+	{
+		Text_StackCount->SetText(FText::AsNumber(Count));
+		Text_StackCount->SetVisibility(ESlateVisibility::Visible);
+	}
+	else
+	{
+		Text_StackCount->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+FGameplayTag UINV_HoverItem::GetItemType() const
+{
+	if (InventoryItem.IsValid())
+	{
+		return InventoryItem->GetItemManifest().GetItemType();
+	}
+	return FGameplayTag::EmptyTag;
+}
+
+void UINV_HoverItem::SetIsStackable(bool bStacks)
+{
+	bIsStackable = bStacks;
+	if (!bStacks)
+	{
+		Text_StackCount->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+UINV_InventoryItem* UINV_HoverItem::GetInventoryItem() const
+{
+	return InventoryItem.Get();
+}
+
+void UINV_HoverItem::SetInventoryItem(UINV_InventoryItem* Item)
+{
+	InventoryItem = Item;
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/UI/Inventory/HoverItem/INV_HoverItem.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Blueprint/UserWidget.h"
+#include "INV_HoverItem.generated.h"
+
+class UTextBlock;
+class UINV_InventoryItem;
+class UImage;
+
+/**
+ * The HoverItem is the item that will appear and follow mouse when inventory item has been clicked
+ */
+UCLASS()
+class INVENTORY_API UINV_HoverItem : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	void SetImageBrush(const FSlateBrush& Brush) const;
+	void UpdateStackCount(const int32 Count) const;
+	
+	FGameplayTag GetItemType() const;
+	FORCEINLINE int32 GetStackCount() const { return StackCount; }
+	FORCEINLINE void SetStackCount(int32 Count) { StackCount = Count; }
+	FORCEINLINE bool IsStackable() const { return bIsStackable; }
+	void SetIsStackable(bool bStacks);
+	FORCEINLINE int32 GetPreviousGridIndex() const { return PreviousGridIndex; }
+	FORCEINLINE void SetPreviousGridIndex(int32 Index) { PreviousGridIndex = Index; }
+	FORCEINLINE FIntPoint GetGridDimensions() const { return GridDimensions; }
+	FORCEINLINE void SetGridDimensions(const FIntPoint Dimensions) { GridDimensions = Dimensions; }
+	UINV_InventoryItem* GetInventoryItem() const;
+	void SetInventoryItem(UINV_InventoryItem* Item);
+	
+private:
+	
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UImage> Image_Icon;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UTextBlock> Text_StackCount;
+	
+	int32 PreviousGridIndex { INDEX_NONE };
+	FIntPoint GridDimensions;
+	TWeakObjectPtr<UINV_InventoryItem> InventoryItem;
+	bool bIsStackable { false };
+	int32 StackCount { 0 };
+};
