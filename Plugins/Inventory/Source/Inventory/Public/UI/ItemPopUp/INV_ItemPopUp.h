f1c208f7dc44974c512129bdf79e9a89a86a48a9
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-02-13 12:33:12 -0700

    Add `UINV_ItemPopUp` widget class with basic UI components
    - Introduced `UINV_ItemPopUp` to handle item-related popup functionality.
    - Added widget components: buttons for Split, Drop, Consume; slider for Split; and supporting text and size box.

---
+++ Plugins/Inventory/Source/Inventory/Public/UI/ItemPopUp/INV_ItemPopUp.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "INV_ItemPopUp.generated.h"
+
+class USizeBox;
+class UTextBlock;
+class USlider;
+class UButton;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UINV_ItemPopUp : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Split;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Drop;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UButton> Button_Consume;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<USlider> Slider_Split;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<UTextBlock> Text_SplitAmount;
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<USizeBox> SizeBox_Root;
+};
