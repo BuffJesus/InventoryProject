157c56c580eb9ba354e85da2f8f5d2803f1b8984
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-02-14 17:49:48 -0700

    Add `UINV_ItemDescription` widget with size retrieval functionality
    - Introduced `UINV_ItemDescription` class to handle item description UI.
    - Added `GetBoxSize` method for retrieving desired size of the description box.
    - Bound `USizeBox` widget for managing size-related functionality.

---
+++ Plugins/Inventory/Source/Inventory/Public/UI/ItemDescription/INV_ItemDescription.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/SizeBox.h"
+#include "INV_ItemDescription.generated.h"
+
+class USizeBox;
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API UINV_ItemDescription : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	FORCEINLINE FVector2D GetBoxSize() const { return SizeBox->GetDesiredSize(); }
+	
+private:
+	UPROPERTY(meta = (BindWidget)) TObjectPtr<USizeBox> SizeBox;
+};
