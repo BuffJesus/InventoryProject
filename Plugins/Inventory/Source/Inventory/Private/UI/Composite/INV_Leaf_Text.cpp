26b0156e3e041a319c0846a4ab599a98baa3651d
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-02-16 09:57:43 -0700

    Add `UINV_Leaf_Text` widget with text and font size handling
    - Implemented `UINV_Leaf_Text` class with `SetText` and `NativePreConstruct` methods for managing text content and font size.
    - Added bindable `Text_LeafText` property and configurable `FontSize` with clamped range.

---
+++ Plugins/Inventory/Source/Inventory/Private/UI/Composite/INV_Leaf_Text.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/Composite/INV_Leaf_Text.h"
+#include "Components/TextBlock.h"
+
+void UINV_Leaf_Text::SetText(const FText& Text) const
+{
+	if (!IsValid(Text_LeafText)) return;
+	Text_LeafText->SetText(Text);
+}
+
+void UINV_Leaf_Text::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+	if (!IsValid(Text_LeafText)) return;
+	
+	FSlateFontInfo FontInfo = Text_LeafText->GetFont();
+	FontInfo.Size = FontSize;
+	Text_LeafText->SetFont(FontInfo);
+}
