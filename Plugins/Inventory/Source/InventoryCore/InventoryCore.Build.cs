108b1e5b22b45027bb2c01f90ffbe879a293ab0c
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-02-17 20:14:09 -0700

    Migrate `FragmentTags` and `ItemTags` to `InventoryCore` module
    - Relocated `INV_FragmentTags` and `INV_ItemTags` definitions from `Inventory` to `InventoryCore` for improved modularity.
    - Updated file paths, namespaces, and build dependencies to reflect the migration.
    - Added `GameplayTags` dependency to `InventoryCore.Build.cs` to enable tag functionality.

--- Plugins/Inventory/Source/InventoryCore/InventoryCore.Build.cs
+++ Plugins/Inventory/Source/InventoryCore/InventoryCore.Build.cs
@@ -10,7 +10,8 @@ public class InventoryCore : ModuleRules
 			new string[]
 			{
 				"Core",
-				"CoreUObject"
+				"CoreUObject",
+				"GameplayTags"
 			}
 		);
 
---
+++ Plugins/Inventory/Source/InventoryCore/Public/Items/INV_ItemTags.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+#define INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(TagName) extern INVENTORYCORE_API FNativeGameplayTag TagName;
+
+namespace GameItems
+{
+	namespace Equipment
+	{
+		namespace Weapons
+		{
+			INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Axe)
+			INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Sword)
+		}
+		namespace Cloaks
+		{
+			INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(RedCloak)
+		}
+		namespace Masks
+		{
+			INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(SteelMask)
+		}
+	}
+	
+	namespace Consumables
+	{
+		namespace Potions
+		{
+			namespace Red
+			{
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Small)
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Large)
+			}
+			namespace Blue
+			{
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Small)
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Large)
+			}
+			namespace Frog
+			{
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Small)
+				INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(Large)
+			}
+		}
+	}
+	
+	namespace Craftables
+	{
+		INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(FireFernFruit)
+		INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(LuminDaisy)
+		INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN(ScorchPedalBlossom)
+	}
+}
+
+#undef INVCORE_DECLARE_GAMEPLAY_TAG_EXTERN
