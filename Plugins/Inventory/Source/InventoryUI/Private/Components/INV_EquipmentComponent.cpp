20a2886516fa674e16b9041c51fdc12aed35864a
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-03-07 10:02:13 -0700

    Add `UINV_EquipmentComponent` for handling equipment logic in `InventoryUI`
    - Introduced `UINV_EquipmentComponent` to manage item equip/unequip logic and integrate with `UINV_InventoryComponent`.
    - Implemented event bindings for `OnItemEquipped` and `OnItemUnequipped`.
    - Added support for initializing inventory and player controller references during `BeginPlay`.

---
+++ Plugins/Inventory/Source/InventoryUI/Private/Components/INV_EquipmentComponent.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/INV_EquipmentComponent.h"
+
+#include "Components/INV_InventoryComponent.h"
+#include "GameFramework/Character.h"
+#include "UI/Utils/INV_InventoryStatics.h"
+
+void UINV_EquipmentComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	OwningPlayerController = Cast<APlayerController>(GetOwner());
+	if (OwningPlayerController.IsValid())
+	{
+		if (const ACharacter* OwnerCharacter = Cast<ACharacter>(OwningPlayerController->GetPawn()); IsValid(OwnerCharacter))
+		{
+			OwnerSkeletalMesh = OwnerCharacter->GetMesh();
+			InitInventoryComponent();
+		}
+	}
+}
+
+void UINV_EquipmentComponent::InitInventoryComponent()
+{
+	InventoryComponent = UINV_InventoryStatics::GetInventoryComponent(OwningPlayerController.Get());
+	if (!InventoryComponent.IsValid()) return;
+	
+	InventoryComponent->OnItemEquipped.AddUniqueDynamic(this, &ThisClass::OnItemEquipped);
+	InventoryComponent->OnItemUnequipped.AddUniqueDynamic(this, &ThisClass::OnItemUnequipped);
+}
+
+
+void UINV_EquipmentComponent::OnItemEquipped(UINV_InventoryItem* EquippedItem)
+{
+	
+}
+
+void UINV_EquipmentComponent::OnItemUnequipped(UINV_InventoryItem* UnequippedItem)
+{
+	
+}
+
---
+++ Plugins/Inventory/Source/InventoryUI/Public/Components/INV_EquipmentComponent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "INV_EquipmentComponent.generated.h"
+
+class UINV_InventoryItem;
+class UINV_InventoryComponent;
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class INVENTORYUI_API UINV_EquipmentComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	TWeakObjectPtr<UINV_InventoryComponent> InventoryComponent;
+	TWeakObjectPtr<APlayerController> OwningPlayerController;
+	TWeakObjectPtr<USkeletalMeshComponent> OwnerSkeletalMesh;
+	
+	UFUNCTION() void OnItemEquipped(UINV_InventoryItem* EquippedItem);
+	UFUNCTION() void OnItemUnequipped(UINV_InventoryItem* UnequippedItem);
+	
+	void InitInventoryComponent();
+};
