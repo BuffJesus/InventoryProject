f07d37e1baafb5667ab75d78da2bcd384c738d15
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-03-08 19:48:03 -0600

    Add `AINV_ProxyMesh` implementation for proxy mesh visualization and delayed initialization logic
    - Implemented `AINV_ProxyMesh` actor to manage proxy mesh visualization in the inventory system.
    - Added retry-based initialization via `AttemptInitialization` and `ScheduleInitializationRetry`.
    - Integrated equipment context setup with `UINV_EquipmentComponent`.

---
+++ Plugins/Inventory/Source/InventoryUI/Public/EquipmentManagement/ProxyMesh/INV_ProxyMesh.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "INV_ProxyMesh.generated.h"
+
+class UINV_EquipmentComponent;
+
+UCLASS()
+class INVENTORYUI_API AINV_ProxyMesh : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AINV_ProxyMesh();
+	void InitializeProxy(APlayerController* PlayerController);
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	// This is the mesh on the player controlled character
+	TWeakObjectPtr<USkeletalMeshComponent> SourceMesh;
+	TWeakObjectPtr<APlayerController> ObservedPlayerController;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UINV_EquipmentComponent> EquipmentComponent;
+
+	// This is the proxy mesh that we will see in the inventory menu
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh;
+
+	FTimerHandle TimerForNextTick;
+	int32 InitializationAttempts { 0 };
+	int32 MaxInitializationAttempts { 30 };
+
+	void AttemptInitialization();
+	void ScheduleInitializationRetry();
+	APlayerController* ResolvePlayerController() const;
+	bool TryInitializeFromPlayerController(APlayerController* PlayerController);
+};
