6af5bff9fee76d7daede7a23812bf48665316e2f
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-03-08 18:54:44 -0600

    Remove extra newline in `INV_ProxyMesh.cpp` for cleaner formatting

--- Plugins/Inventory/Source/InventoryCore/Private/EquipmentManagement/ProxyMesh/INV_ProxyMesh.cpp
+++ Plugins/Inventory/Source/InventoryCore/Private/EquipmentManagement/ProxyMesh/INV_ProxyMesh.cpp
@@ -1,6 +1,5 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "EquipmentManagement/ProxyMesh/INV_ProxyMesh.h"
 #include "InventoryUI/Public/Components/INV_EquipmentComponent.h"
 
