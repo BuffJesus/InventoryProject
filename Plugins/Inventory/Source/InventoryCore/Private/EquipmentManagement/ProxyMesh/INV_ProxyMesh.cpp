70ad1375505997885b778437f607dd1dc79db26f
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-03-08 18:49:18 -0600

    Add `AINV_ProxyMesh` actor for proxy mesh visualization in inventory system

---
+++ Plugins/Inventory/Source/InventoryCore/Private/EquipmentManagement/ProxyMesh/INV_ProxyMesh.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EquipmentManagement/ProxyMesh/INV_ProxyMesh.h"
+#include "InventoryUI/Public/Components/INV_EquipmentComponent.h"
+
+AINV_ProxyMesh::AINV_ProxyMesh()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	Mesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+	EquipmentComponent = CreateDefaultSubobject<UINV_EquipmentComponent>(TEXT("Equipment"));
+}
+
+void AINV_ProxyMesh::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
---
+++ Plugins/Inventory/Source/InventoryCore/Public/EquipmentManagement/ProxyMesh/INV_ProxyMesh.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "INV_ProxyMesh.generated.h"
+
+class UINV_EquipmentComponent;
+
+UCLASS()
+class INVENTORYCORE_API AINV_ProxyMesh : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AINV_ProxyMesh();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	// This is the mesh on the player controlled character
+	TWeakObjectPtr<USkeletalMeshComponent> SourceMesh;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UINV_EquipmentComponent> EquipmentComponent;
+	
+	// This is the proxy mesh that we will see in the inventory menu
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh;
+};
