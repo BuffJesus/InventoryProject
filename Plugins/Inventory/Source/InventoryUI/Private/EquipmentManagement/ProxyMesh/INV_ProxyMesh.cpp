cae32e8bc4bf329d4704ce6728cc25d5178fa709
BuffJesus <69093219+BuffJesus@users.noreply.github.com>
2026-03-08 20:12:06 -0600

    Remove redundant proxy mesh Z-location adjustment logic and delete outdated assets

--- Plugins/Inventory/Source/InventoryUI/Private/EquipmentManagement/ProxyMesh/INV_ProxyMesh.cpp
+++ Plugins/Inventory/Source/InventoryUI/Private/EquipmentManagement/ProxyMesh/INV_ProxyMesh.cpp
@@ -96,16 +96,6 @@ bool AINV_ProxyMesh::TryInitializeFromPlayerController(APlayerController* Player
 	ObservedPlayerController = PlayerController;
 	SourceMesh = CharacterMesh;
 	Mesh->SetSkeletalMesh(SourceMesh->GetSkeletalMeshAsset());
-	Mesh->SetRelativeTransform(SourceMesh->GetRelativeTransform());
-
-	if (const USkeletalMesh* SkeletalMeshAsset = SourceMesh->GetSkeletalMeshAsset())
-	{
-		const FBoxSphereBounds MeshBounds = SkeletalMeshAsset->GetBounds();
-		const float MeshBottomZ = MeshBounds.Origin.Z - MeshBounds.BoxExtent.Z;
-		FVector RelativeLocation = Mesh->GetRelativeLocation();
-		RelativeLocation.Z -= MeshBottomZ * Mesh->GetRelativeScale3D().Z;
-		Mesh->SetRelativeLocation(RelativeLocation);
-	}
 
 	if (UAnimInstance* SourceAnimInstance = SourceMesh->GetAnimInstance())
 	{
